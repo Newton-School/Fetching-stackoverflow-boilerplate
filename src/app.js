bb63210a863d60f4d11f8ef13750225595f95d65
swaraj-jain <happy2000jain@gmail.com>
2023-04-15 12:57:52 +0530

    boiler plate added

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+// Importing discussions from discussions.json file
+const discussions = JSON.parse(fs.readFileSync(`data/blog.json`));
+
+// Middlewares
+app.use(express.json());
+
+// Write GET endpoint for sending all the discussions to user here
+
+/*
+
+Endpoint - /api/v1/blogs
+
+Return 200 Status code
+json = {
+  discussions: [
+    {
+      id: 1,
+      heading: 'Binary Search',
+      body: 'How we can prove time complexity of BS is log(n) ?',
+      tags: 'msbksbn'
+      creator_id: 'xyz1'
+    },
+    {
+      id: 2,
+      heading: 'Binary Search',
+      body: 'Is BS a imp topic ?',
+      tags: 'msbksbn'
+      creator_id: 'xyz2'
+    }
+  ]
+}
+
+*/
+
+
+module.exports = app;
